e9e8d86811871fcb1d33e4e8e834a035798b8506
BiweiZhu <84614424+ZHUBIWEI@users.noreply.github.com>
2022-11-17 12:19:47 +0000

    Hessian matrix and some comments

--- proj4.R
+++ proj4.R
@@ -25,76 +25,7 @@ newt <- function(theta,func,grad,hess=NULL,...,
       (t(hfd) + hfd)/2
     }
   }
-  
-  th0 <- theta
-  f0 <- func(th0,...)
-  g0 <- grad(th0,...)
-  h0 <- hess(th0,...)
-  
-  # Check if the initial theta is suitable for optimization
-  # Check if objective function value is -inf
-  if (is.infinite(f0) & f0 < 0)
-    stop("Function is unbounded below.")
-  # Check if objective or derivatives are not finite
-  else if (is.infinite(f0) | any(is.infinite(g0)) | any(is.infinite(h0)))
-    stop("Objective or derivatives are not finite at the initial theta.")
-  
-  it <- 0
-  while (any(abs(g0) >= tol*abs(f0+fscale))) {
-    
-    
-    if (it == maxit)
-      stop("Maxit is reached without convergence.")
-    
-    
-    # If h0 is not positive definite then perturb it to be
-    # by adding a multiple of indentity matrix
-    if (inherits(try(chol(h0),silent=TRUE),"try-error")) {
-      m <- (1e-6)*norm(h0) # Multiplier of indentity matrix
-      # cat(m)
-      while (inherits(try(chol(h0),silent=TRUE),"try-error")) {
-        diag(h0) <- diag(h0) + m
-        m <- 10*m
-        cat(m)
-      }
-    }
-      
-    
-    # Calculate the step
-    R <- chol(h0)
-    step <- -backsolve(R,forwardsolve(t(R),g0))
-    
-    # Step halving to make sure step reduce the objective
-    for (it_half in 0:max.half) {
-      th1 <- th0 + step
-      f1 <- func(th1,...)
-      if (is.infinite(f1) & f1 < 0)
-        stop("Function is unbounded below.")
-      else if (f1 >= f0) step <- step/2
-      else break
-    }
-    
-    if (f1 >= f0)
-      stop("Step fails to reduce the objective after max.half step halvings")
-    
-    
-    # Update optimization parameters
-    th0 <- th1
-    f0 <- f1
-    g0 <- grad(th1,...)
-    h0 <- hess(th1,...)
-    it <- it + 1
-  }
-  if (inherits(try(chol(h0),silent=TRUE),"try-error")) {
-    warning("Hessian is not positive definite at convergence, 
-                its inverse is not returned.")
-    return(list(f=f0,theta=th0,iter=it,g=g0))
-  }
-  else {
-    R <- chol(h0)
-    return(list(f=f0,theta=th0,iter=it,g=g0,
-                Hi=backsolve(R,forwardsolve(t(R),diag(n)))))
-  }
+
   
 }
 
